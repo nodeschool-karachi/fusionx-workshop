8cefd03581babebb1456f34972fed89668d54494
Alqama Bin Sadiq <alqamabinsadiq@gmail.com>
2019-09-16 03:55:31 +0500

    refactor and add connec the new getMessage func.

--- client/src/components/streaming_box/messages_container/MessagesContainer.index.js
+++ client/src/components/streaming_box/messages_container/MessagesContainer.index.js
@@ -1,12 +1,12 @@
-import React, { Component } from "react";
-import Message from "./message/Message.index";
-import { getMessage } from "../../../utils/getMessages";
+import React, { Component } from 'react';
+import Message from './message/Message.index';
+import { getMessage } from '../../../utils/getMessages';
 
 class Chat extends Component {
   // Initial State.
   state = {
-    messages: [getMessage()],
-    isStreaming: this.props.isStreaming
+    messages: [],
+    isStreaming: this.props.isStreaming,
   };
 
   componentDidMount() {
@@ -15,13 +15,13 @@ class Chat extends Component {
         this.generateMessage();
       }
     }, 1000);
-    document.addEventListener("keydown", this.handleKey);
+    document.addEventListener('keydown', this.handleKey);
   }
 
   static getDerivedStateFromProps(props, state) {
     if (props.isStreaming !== state.isStreaming) {
       return {
-        isStreaming: props.isStreaming
+        isStreaming: props.isStreaming,
       };
     }
     return null;
@@ -29,39 +29,36 @@ class Chat extends Component {
 
   getSnapshotBeforeUpdate(prevProps, prevState) {
     const { current } = this.ulRef;
-    console.log(current.scrollTop, current.clientHeight, current.scrollHeight);
     return {
       shouldAutoScroll:
-        current.scrollTop + current.clientHeight >= current.scrollHeight
+        current.scrollTop + current.clientHeight >= current.scrollHeight,
     };
   }
 
   componentDidUpdate(prevProps, prevState, { shouldAutoScroll }) {
-    // console.log(current.scrollTop);
     if (shouldAutoScroll) {
       const { current } = this.ulRef;
-      console.log(current.scrollTop);
       current.scrollTop = current.scrollHeight;
     }
   }
 
   componentWillUnmount() {
     clearInterval(this.timerID);
-    document.removeEventListener("keydown", this.handleKey);
+    document.removeEventListener('keydown', this.handleKey);
   }
 
   // fetch the new messages.
-  generateMessage = () => {
-    const message = getMessage();
+  generateMessage = async () => {
+    const message = await getMessage();
     this.setState(prevState => ({
-      messages: [...prevState.messages, message]
+      messages: [...prevState.messages, message],
     }));
   };
 
   // Handles the keystroke.
   handleKey = e => {
-    if (e.key === "c") {
-      this.setState({ messages: [getMessage()] });
+    if (e.key === 'c') {
+      this.setState({ messages: [] });
     }
   };
 
@@ -76,6 +73,7 @@ class Chat extends Component {
           {this.state.messages.map((message, index) => {
             return <Message {...message} key={index} />;
           })}
+          {this.state.messages.length == 0 && <div>No data</div>}
         </ul>
       </div>
     );
