ba534c83d70fc1bea0f577330cee87cd7d838d1f
Alqama Bin Sadiq <alqamabinsadiq@gmail.com>
2019-09-17 05:56:12 +0500

    convert streamingBox using useState hook

--- client/src/components/streaming_box/StreamingBox.index.js
+++ client/src/components/streaming_box/StreamingBox.index.js
@@ -1,34 +1,32 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import MessagesContainer from './messages_container/MessagesContainer.index';
 import SectionHeader from './section_header/SectionHeader.index';
 import StreamingButton from './streaming_button/StreamingButton.index';
-// import Logo from "../logo/Logo.index";
-class StreamingBox extends Component {
-  state = {
-    isStreaming: false,
-  };
+
+const StreamingBox = () => {
+  /**
+   * isStreaming contains the value
+   * setIsStraming is used to update the isStreaming variable.
+   */
+  const [isStreaming, setIsStreaming] = useState(false);
 
   // Handles the stream.
-  handleStreaming = e => {
-    this.setState(prevState => ({
-      isStreaming: !prevState.isStreaming,
-    }));
+  const handleStreaming = () => {
+    setIsStreaming(value => !value);
   };
 
-  render() {
-    return (
-      <div className="streaming-box">
-        <SectionHeader />
-        <MessagesContainer isStreaming={this.state.isStreaming} />
-        <div className="streaming-box__button-container l-container l-container--flex ">
-          <StreamingButton
-            isStreaming={this.state.isStreaming}
-            handleStreaming={this.handleStreaming}
-          />
-        </div>
+  return (
+    <div className="streaming-box">
+      <SectionHeader />
+      <MessagesContainer isStreaming={isStreaming} />
+      <div className="streaming-box__button-container l-container l-container--flex ">
+        <StreamingButton
+          isStreaming={isStreaming}
+          handleStreaming={handleStreaming}
+        />
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default StreamingBox;
